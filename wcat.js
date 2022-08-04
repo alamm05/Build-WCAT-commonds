2cc8195870bcd63b0f3932507809d252916a2a0d
Md Rashid Alam <97775228+alamm05@users.noreply.github.com>
2022-08-04 17:08:03 +0530

    Add files via upload

---
+++ wcat.js
@@ -0,0 +1,110 @@
+//1) node wcat.js filepath => displays the contents of a file in terminal
+//2) node wcat.js filepath1 filepath2 filepath3 => displays the content of all files in terminal in concatinated form in given order
+//3) node wcat.js -n file1 file2 file3  
+// node wcat.js f1.txt
+//node wcat.js f1.txt f2.txt f3.txt
+const fs = require("fs")
+let inputArr = process.argv.slice(2);
+//console.log(input);
+
+console.log(inputArr);
+let filesArr = [];
+let optionsArr = [];
+//----------->placed path in filesArr<-------
+for(let i =0; i<inputArr.length;i++){
+    let firstChar = inputArr[i].charAt(0);
+    console.log(firstChar);
+    if(firstChar == '-'){
+        optionsArr.push(inputArr[i])
+    }
+    else{
+        filesArr.push(inputArr[i]);
+    }
+   
+}
+//console.log("file to be read"+filesArr);
+
+//check if all files are present //
+for(let i = 0; i<filesArr.length;i++){
+    let doesExist = fs.existsSync(filesArr[i]);
+    if(!doesExist){
+        console.log("files does not exist");
+        //return;
+        process.exit(); 
+    }
+} 
+
+//content read and appending starts
+let content = "";
+for(let i =0; i<filesArr.length ;i++){
+    let fileContent =fs.readFileSync(filesArr[i]);
+    content+=fileContent+"\r\n";
+}
+console.log(content);
+let contentArr = content.split("\r\n");
+console.table(contentArr);
+
+//check if -s is present or not
+let tempArr = [];
+let isPresent = optionsArr.includes("-s");
+if(isPresent){
+    for(let i =0; i<contentArr.length; i++){
+        if(contentArr[i] =="" && contentArr[i-1]==""){
+            contentArr[i] = null;
+        }
+        else if(contentArr[i]=="" && contentArr[i-1]==null){
+            contentArr[i] = null;
+        }
+    }
+    console.table(contentArr);
+    
+    //push everything i tempArr except null
+    for(let i=0;i<contentArr.length;i++){
+        if(contentArr[i] != null){
+            tempArr.push(contentArr[i]);
+        }
+    }
+    console.log("data after removing extra line\n",tempArr);
+
+}
+contentArr =tempArr;
+
+let indexOfN = optionsArr.indexOf("-n");
+let indexOfB = optionsArr.indexOf("-b");
+//if -n or -b is not found, -1 is returned
+
+let finalOption =" ";
+//both -n and -b is present
+if(indexOfN != -1 &&indexOfB !=-1){
+    if(indexOfN<indexOfB){
+        finalOption = "-n";
+    }
+    else{
+        finalOption ="-b";
+    }
+}
+//either -n is present or -b is present 
+else{
+    if(indexOfN !=-1){
+        finalOption ="-n";
+    }
+    else if(indexOfB !=-1){
+        finalOption ="-b";
+    }
+}
+
+//calling of function by evaluating  finaloption
+if(finalOption == "-n"){
+    modifyContentByN();
+}
+else if(finalOption ="-b"){
+    modifyContentByB();
+}
+function modifyContentByN(){
+
+}
+function modifyContentByB(){
+
+}
+
+console.log(contentArr);
